ca727f59b06e63938c03ffc9263a574915143498
Kevin Michel <kevin.michel@aiven.io>
2021-05-15 01:49:39 +0200

    add tests for PostgreSQL 13

--- ci/install.sh
+++ ci/install.sh
@@ -9,12 +9,12 @@ dpkg-reconfigure --frontend noninteractive tzdata
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get -y update
-apt-get install -y postgresql-{9.5,9.6,10,11,12} postgresql-{9.5,9.6,10,11,12}-postgis-3 postgresql-{9.5,9.6,10,11,12}-pgextwlist
+apt-get install -y postgresql-{9.5,9.6,10,11,12,13} postgresql-{9.5,9.6,10,11,12,13}-postgis-3 postgresql-{9.5,9.6,10,11,12,13}-pgextwlist
 git clone https://github.com/aiven/aiven-extras /aiven-extras
 make -C /aiven-extras clean rpm
 # maybe add a deb target to aiven-extras in the future, but for now, while hacky, this is (probably) terser and less intrusive
 
-for dest in "9.6" "10" "11" "12"
+for dest in "9.6" "10" "11" "12" "13"
 do
     cp /aiven-extras/build/aiven_extras.control /usr/share/postgresql/${dest}/extension/
     cp /aiven-extras/sql/*.sql /usr/share/postgresql/${dest}/extension/
